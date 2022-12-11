f20a88ef1b88684c4c5e67c5df60c3e8b72fcfd8
Alberto Valverde <77863587+asvalverde01@users.noreply.github.com>
2022-12-11 15:44:59 -0500

    Azure login

--- VinuezaValverdeVintimilla_P2/Views/Principal.xaml.cs
+++ VinuezaValverdeVintimilla_P2/Views/Principal.xaml.cs
@@ -1,8 +1,12 @@
+using Microsoft.Graph;
+
 namespace VinuezaValverdeVintimilla_P2.Views;
 
 public partial class Principal : ContentPage
 {
-	public Principal()
+    private GraphService graphService;
+    private User user;
+    public Principal()
 	{
 		InitializeComponent();
 	}
@@ -10,5 +14,15 @@ public partial class Principal : ContentPage
 	private void OnCounterClicked(object sender, EventArgs e)
 	{
 		Navigation.PushAsync(new InicioSesion());
+    }
+
+    private async void GetUserInfoBtn_Clicked(object sender, EventArgs e)
+    {
+        if (graphService == null)
+        {
+            graphService = new GraphService();
+        }
+        user = await graphService.GetMyDetailsAsync();
+        HelloLabel.Text = $"Hello, {user.DisplayName}!";
     }
 }
