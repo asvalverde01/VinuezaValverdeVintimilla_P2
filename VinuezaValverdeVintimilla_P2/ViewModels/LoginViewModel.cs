700e998fa29c2c16cad6a1de9a16360d2fd5b8b5
Alberto Valverde <77863587+asvalverde01@users.noreply.github.com>
2023-01-29 16:51:19 -0500

    Firebase auth added

---
+++ VinuezaValverdeVintimilla_P2/ViewModels/LoginViewModel.cs
@@ -0,0 +1,84 @@
+﻿using Android.Content.Res;
+using Firebase.Auth;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VinuezaValverdeVintimilla_P2.Views;
+
+namespace VinuezaValverdeVintimilla_P2.ViewModels
+{
+    internal class LoginViewModel : INotifyPropertyChanged
+    {
+        public string webApiKey = "AIzaSyDNxP1WhGmdE1hfuX2vulZCpnWIj4VKvAc";
+
+        private INavigation _navigation;
+        private string userName;
+        private string userPassword;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public Command RegisterBtn { get; }
+        public Command LoginBtn { get; }
+
+        public string UserName
+        {
+            get => userName; set
+            {
+                userName = value;
+                RaisePropertyChanged("UserName");
+            }
+        }
+
+        public string UserPassword
+        {
+            get => userPassword; set
+            {
+                userPassword = value;
+                RaisePropertyChanged("UserPassword");
+            }
+        }
+
+        public LoginViewModel(INavigation navigation)
+        {
+            this._navigation = navigation;
+            RegisterBtn = new Command(RegisterBtnTappedAsync);
+            LoginBtn = new Command(LoginBtnTappedAsync);
+        }
+
+        private async void LoginBtnTappedAsync(object obj)
+        {
+            var authProvider = new FirebaseAuthProvider(new FirebaseConfig(webApiKey));
+            try
+            {
+                var auth = await authProvider.SignInWithEmailAndPasswordAsync(UserName, UserPassword);
+                var content = await auth.GetFreshAuthAsync();
+                var serializedContent = JsonConvert.SerializeObject(content);
+                Preferences.Set("FreshFirebaseToken", serializedContent);
+                //await this._navigation.PushAsync(new Inicio());
+                //Navigation.PushAsync(new AppShell());
+                Application.Current.MainPage = new AppShell();
+            }
+            catch (Exception ex)
+            {
+                await App.Current.MainPage.DisplayAlert("Alert", ex.Message, "OK");
+                
+            }
+
+
+        }
+
+        private async void RegisterBtnTappedAsync(object obj)
+        {
+            await this._navigation.PushAsync(new RegisterPage());
+        }
+
+        private void RaisePropertyChanged(string v)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(v));
+        }
+    }
+}
--- VinuezaValverdeVintimilla_P2/Views/Perfil.xaml.cs
+++ VinuezaValverdeVintimilla_P2/Views/Perfil.xaml.cs
@@ -1,3 +1,5 @@
+using Newtonsoft.Json;
+
 namespace VinuezaValverdeVintimilla_P2.Views;
 
 public partial class Perfil : ContentPage
@@ -5,5 +7,7 @@ public partial class Perfil : ContentPage
 	public Perfil()
 	{
 		InitializeComponent();
-	}
+        var userInfo = JsonConvert.DeserializeObject<Firebase.Auth.FirebaseAuth>(Preferences.Get("FreshFirebaseToken", ""));
+        UserEmail.Text = userInfo.User.Email;
+    }
 }
