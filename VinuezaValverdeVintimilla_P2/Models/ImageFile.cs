cad143af9cfed4e65d25a2c569abc6717dc838b2
Thyara <thyara.vintimilla@udla.edu.ec>
2023-01-31 23:40:30 -0600

    Cambio5

--- VinuezaValverdeVintimilla_P2/Models/ImageFile.cs
+++ VinuezaValverdeVintimilla_P2/Models/ImageFile.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 
 namespace VinuezaValverdeVintimilla_P2.Models
 {
+    //Necesario para mostrar publicaciones
     public class ImageFile
     {
         public string byteBase64 { get; set; }
